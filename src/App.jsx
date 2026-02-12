60429b27de244ccd13a7443259ac944e3bd12630
ivanramirez-git <ivanrene10@gmail.com>
2026-02-12 08:23:44 -0500

    fix: import useMemo for SPA pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import { Routes, Route, Link } from 'react-router-dom';
 import {
   Globe,
