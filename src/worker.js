ab8b0419a7743ea46d63fc910b56eaf9a8ed0d44
ivanramirez-git <ivanrene10@gmail.com>
2026-02-11 20:55:21 -0500

    chore: add Cloudflare Workers deploy config

---
+++ src/worker.js
@@ -0,0 +1,17 @@
+export default {
+  async fetch(request, env) {
+    const response = await env.ASSETS.fetch(request);
+
+    if (response.status !== 404) {
+      return response;
+    }
+
+    const url = new URL(request.url);
+    const indexRequest = new Request(
+      new URL('/index.html', url.origin),
+      request
+    );
+
+    return env.ASSETS.fetch(indexRequest);
+  }
+};
